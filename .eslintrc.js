91423062f07b50792d84ac5b44619fb2e940fb9e
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-02 04:32:26 +0900

    env : add eslint plugins and rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,10 @@ module.exports = {
     browser: true,
     node: true,
   },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
 };
--- src/controller/user-controller.ts
+++ src/controller/user-controller.ts
@@ -1,12 +1,12 @@
 import User from 'model/user-model';
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 
-export async function createUser(
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) {
+export async function createUser(req: Request, res: Response): Promise<any> {
   const { loginId, nickname, password } = req.body;
-  const insertId = await User.createUser({ loginId, nickname, password });
+  const insertId = await User.createUser({
+    loginId,
+    nickname,
+    password,
+  });
   res.status(201).json(insertId);
 }
