b607a8e4916bfdb846d55d3be3741ec169f30940
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-15 18:30:54 +0900

    feat : update square sql query

--- src/model/square-model.ts
+++ src/model/square-model.ts
@@ -23,9 +23,36 @@ class SquareRepo {
   static async findAllReviews(): Promise<Review[]> {
     const query = `
       SELECT
-        *
-      FROM
-        review
+        review.id id, 
+        review.title title, 
+        review.content content, 
+        user.nickname reviewer, 
+        review.created_at created_at, 
+        review.updated_at updated_at, 
+        book.title book_title, 
+        book.author book_author, 
+        salon.name salon, 
+        space.name space 
+      FROM 
+        review 
+      JOIN 
+        book 
+      ON 
+        review.book_id=book.id 
+      JOIN 
+       user 
+      ON 
+        review.user_id=user.id 
+      LEFT JOIN
+        salon 
+      ON 
+        book.salon_id=salon.id 
+      LEFT JOIN 
+        space 
+      ON 
+        book.space_id=space.id
+      ORDER BY
+        review.updated_at
     `;
     const reviews = await queryExecutor(query);
     return reviews;
@@ -34,9 +61,36 @@ class SquareRepo {
   static async findAllQuotes(): Promise<Quote[]> {
     const query = `
       SELECT
-        *
-      FROM
-        quote
+        quote.id id, 
+        quote.content content, 
+        quote.page page, 
+        user.nickname quoter, 
+        quote.created_at created_at, 
+        quote.updated_at updated_at, 
+        book.title book_title, 
+        book.author book_author, 
+        salon.name salon, 
+        space.name space 
+      FROM 
+        quote 
+      JOIN 
+        book 
+      ON 
+        quote.book_id=book.id 
+      JOIN 
+        user 
+      ON 
+        quote.user_id=user.id 
+      LEFT JOIN
+        salon 
+      ON 
+        book.salon_id=salon.id 
+      LEFT JOIN 
+        space 
+      ON 
+        book.space_id=space.id
+      ORDER BY
+        quote.updated_at
     `;
     const quotes = await queryExecutor(query);
     return quotes;
