5b1c20fbcf0110790aea0ef08c1a1e9ef3d0d3a7
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-03-16 18:53:51 +0900

    feat : add Salon routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,13 @@ import { Router } from 'express';
 import userRouter from 'Routes/user-routes';
 import squareRouter from 'Routes/square-routes';
 import spaceRouter from 'Routes/space-routes';
-// import salonRouter from 'Routes/salon-routes';
+import salonRouter from 'Routes/salon-routes';
 
 const router = Router();
 
 router.use('/user', userRouter);
 router.use('/square', squareRouter);
 router.use('/space', spaceRouter);
-// router.use('/salon', salonRouter);
+router.use('/salon', salonRouter);
 
 export default router;
--- src/routes/salon-routes.ts
+++ src/routes/salon-routes.ts
@@ -1,28 +1,138 @@
-// import { Router } from 'express';
-// import {
-//   getCurrentUser,
-//   signUpByLoginId,
-//   signInByLoginId,
-// } from 'src/service/user-service';
-// import { validateBody } from 'Middlewares/validate-body';
-// import { decodeJWT } from 'Middlewares/decode-jwt';
-// import passport from 'Utils/passport';
+import { Router } from 'express';
+import SalonService from 'Service/salon-service';
+import { validateBody } from 'Middlewares/validate-body';
+import { decodeJWT } from 'Middlewares/decode-jwt';
+import { checkPermission } from 'Middlewares/check-permission';
 
-// const salonRouter = Router();
+const salonRouter = Router();
 
-// salonRouter.post(
-//   '/signUp',
-//   validateBody<SignUpUserBody>(['loginId', 'nickname', 'password']),
-//   signUpByLoginId,
-// );
+export interface UpdateSalonBody {
+  name: string;
+}
 
-// salonRouter.post(
-//   '/signIn',
-//   validateBody<SignInUserBody>(['loginId', 'password']),
-//   passport.authenticate('local', { session: false }),
-//   signInByLoginId,
-// );
+salonRouter.get('/:salonId', SalonService.getsalon);
+salonRouter.patch(
+  '/:salonId',
+  decodeJWT,
+  checkPermission,
+  validateBody<UpdateSalonBody>(['name']),
+  SalonService.updateSalon,
+);
 
-// salonRouter.get('/', decodeJWT, getCurrentUser);
+export interface AddBookBody {
+  title: string;
+  author: string;
+  description: string;
+}
 
-// export default salonRouter;
+export interface AddReviewBody {
+  title: string;
+  content: string;
+}
+
+export type updateReviewBody = AddReviewBody;
+
+export interface addReviewCommentBody {
+  comment: string;
+}
+
+export type updateReviewCommentBody = addReviewCommentBody;
+
+export interface AddQuoteBody {
+  content: string;
+  page: number;
+}
+
+export type updateQuoteBody = AddQuoteBody;
+
+export interface addQuoteCommentBody {
+  comment: string;
+}
+
+export type updateQuoteCommentBody = addQuoteCommentBody;
+
+salonRouter.get('/:salonId/book/:bookId', SalonService.getBook);
+salonRouter.post(
+  '/:salonId/book',
+  decodeJWT,
+  checkPermission,
+  validateBody<AddBookBody>(['title', 'author', 'description']),
+  SalonService.addBook,
+);
+salonRouter.delete('/:salonId/book/:bookId', decodeJWT, checkPermission, SalonService.deleteBook);
+
+salonRouter.post(
+  '/:salonId/book/:bookId/review',
+  decodeJWT,
+  checkPermission,
+  validateBody<AddReviewBody>(['title', 'content']),
+  SalonService.addReview,
+);
+salonRouter.patch(
+  '/:salonId/book/:bookId/review/:reviewId',
+  decodeJWT,
+  checkPermission,
+  validateBody<updateReviewBody>(['title', 'content']),
+  SalonService.updateReview,
+);
+salonRouter.delete('/:salonId/book/:bookId/review/:reviewId', decodeJWT, checkPermission, SalonService.deleteReview);
+
+salonRouter.get('/:salonId/book/:bookId/review/:reviewId/comment', SalonService.getAllReviewComments);
+salonRouter.post(
+  '/:salonId/book/:bookId/review/:reviewId/comment',
+  decodeJWT,
+  validateBody<addReviewCommentBody>(['comment']),
+  SalonService.addReviewComment,
+);
+salonRouter.patch(
+  '/:salonId/book/:bookId/review/:reviewId/comment/:commentId',
+  decodeJWT,
+  checkPermission,
+  validateBody<updateReviewCommentBody>(['comment']),
+  SalonService.updateReviewComment,
+);
+salonRouter.delete(
+  '/:salonId/book/:bookId/review/:reviewId/comment/:commentId',
+  decodeJWT,
+  checkPermission,
+  SalonService.deleteReviewComment,
+);
+
+salonRouter.post(
+  '/:salonId/book/:bookId/quote',
+  decodeJWT,
+  checkPermission,
+  validateBody<AddQuoteBody>(['content', 'page']),
+  SalonService.addQuote,
+);
+salonRouter.patch(
+  '/:salonId/book/:bookId/quote/:quoteId',
+  decodeJWT,
+  checkPermission,
+  validateBody<updateQuoteBody>(['content', 'page']),
+  SalonService.updateQuote,
+);
+salonRouter.delete('/:salonId/book/:bookId/quote/:quoteId', decodeJWT, checkPermission, SalonService.deleteQuote);
+
+salonRouter.get('/:salonId/book/:bookId/quote/:quoteId/comment', SalonService.getAllQuoteComments);
+salonRouter.post(
+  '/:salonId/book/:bookId/quote/:quoteId/comment',
+  decodeJWT,
+  validateBody<addQuoteCommentBody>(['comment']),
+  SalonService.addQuoteComment,
+);
+salonRouter.patch(
+  '/:salonId/book/:bookId/quote/:quoteId/comment/:commentId',
+  decodeJWT,
+  checkPermission,
+  validateBody<updateQuoteCommentBody>(['comment']),
+  SalonService.updateQuoteComment,
+);
+salonRouter.delete(
+  '/:salonId/book/:bookId/quote/:quoteId/comment/:commentId',
+  decodeJWT,
+  checkPermission,
+  SalonService.deleteQuoteComment,
+);
+
+export default salonRouter;
