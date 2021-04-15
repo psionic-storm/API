ac1de8e649788995bf5ae4eef92d6032d3a23305
jungcome7 <34447105+jungcome7@users.noreply.github.com>
2021-04-15 18:32:03 +0900

    chore : remove error constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,17 +8,3 @@ export const STATUS_CODE = {
   CONFLICT: 409,
   SERVER_ERROR: 500,
 };
-
-export const ERROR_CODE = {
-  DUPLICATE_ID: 'duplicate_id',
-};
-
-export const ERROR_MSG = {
-  DUPLICATE_ID: 'ID already exists',
-};
-
-export const ERROR_JSON = {
-  DUPLICATE_ID: {
-    error: { status: ERROR_CODE.DUPLICATE_ID, message: ERROR_MSG.DUPLICATE_ID },
-  },
-};
